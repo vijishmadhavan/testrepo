b35c4279257540731577934220f700576986afbc
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2022-04-01 17:09:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from fastai.vision import *
+
+
+MODEL_URL = "https://www.dropbox.com/s/eyhud3qtk6pzztd/testapp.pkl?dl=0"
+urllib.request.urlretrieve(MODEL_URL, "testapp.pkl")
+path = Path(".")
+learn=load_learner(path, 'testapp.pkl')
+
+uploaded_file = st.file_uploader("Choose an image...")
+p,img_hr,b = learn.predict(uploaded_file)
+x = np.minimum(np.maximum(image2np(img_hr.data*255), 0), 255).astype(np.uint8)
+img = PIL.Image.fromarray(x)
+st.image(img, caption='Sunrise by the mountains')
